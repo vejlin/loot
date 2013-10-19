2112e1519c7b8ba51a28f15823a5159b41329b06
vejlin <janvejlin@hotmail.com>
2013-10-19 23:30:51 +0200

    Create Tables.js

---
+++ javascripts/Tables.js
@@ -0,0 +1,81 @@
+//Side 48 Fantasy Companion
+
+//Treasure Tables
+//Name          Gold Amount         Magic Item Chance
+Meager           10-100) " Gold"         0.01
+Worthwhile      100-1000 " Gold"         0.25
+Rich            500-5000 " Gold"         0.50
+Treasure Trove 1000-10000 " Gold"        1.0
+
+//print XX Gold + Magic Item from tables
+
+//Magic Item Table
+//d20 Table
+1 Table1A
+2 Table1A
+3 Table2A
+4 Table2A
+5 Table2A
+6 Table3A
+7 Table3A
+8 Table4A
+9 Table4A
+10 Table4A
+11 Table4A
+12 Table5
+13 Table5
+14 Table5
+15 Table6
+16 Table6
+17 Table7A
+18 Table7A
+19 Table8
+20 Table9A
+
+//Table 1A: Armor Type
+//d20 Type
+1 Table1B
+2 Table1B
+3 Table1B
+4 Table1B
+5 Table1B
+6 Table1B
+7 Table1B
+8 Table1B
+9 Table1B
+10 Table1B
+11 Table1B
+12 Table1B
+13 Table1B
+14 Table1B
+15 Table1B
+16 Table1D
+17 Table1D
+18 Table1D
+19 Table1D
+20 Table1J
+
+//Table 1B: Armor
+//Roll a d20 to determine the exact armor type, then roll on Table 1C to determine the specific bonus.
+d20 Cost Type
+1 "$50 Leather" + Table1C
+2 "$50 Leather" + Table1C
+3 "$50 Leather" + Table1C
+4 "$50 Leather" + Table1C
+5 "$50 Leather" + Table1C
+6 "$50 Leather" + Table1C
+7 "$50 Leather" + Table1C
+8 "$300 Chain hauberk" + Table1C
+9 "$300 Chain hauberk" + Table1C
+10 "$300 Chain hauberk" + Table1C
+11 "$300 Chain hauberk" + Table1C
+12 "$300 Chain hauberk" + Table1C
+13 "$400 Plate corselet" + Table1C
+14 "$400 Plate corselet" + Table1C
+15 "$400 Plate corselet" + Table1C
+16 "$200 Plate arms (vambrace)" + Table1C
+17 "$300 Plate leggings (greaves)" + Table1C
+18 "$75 Pot Helm" + Table1C
+19 "$75 Pot Helm" + Table1C
+20 "$150 Steel Helmet" + Table1C
+
